f7640154db486c776714a51fd5fcb7f65ca2d4fa
dongjune.youn <exwaiz@live.com>
2023-07-05 16:58:49 +0900

    [feature] guess() 27p

--- BaseBall-17/baseball.cpp
+++ BaseBall-17/baseball.cpp
@@ -1,1 +1,13 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class Baseball
+{
+public:
+	string guess(const string & str)
+	{
+		throw length_error("Must be three letters.");
+		return "12";
+	}
+};
--- GoogleTest/test.cpp
+++ GoogleTest/test.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
+#include "../BaseBall-17/baseball.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
